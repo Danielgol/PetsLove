934c16225602c5301e1561f4b700e743af70ebfb
Daniel Rosa <danielgol@yahoo.com.br>
2019-11-03 14:52:12 -0300

    Criando metodos listar (Usuarios e Animais)

--- src/PetsLove/sistema/FachadaPL.java
+++ src/PetsLove/sistema/FachadaPL.java
@@ -1,7 +1,10 @@
 package PetsLove.sistema;
 
+import java.util.ArrayList;
+
 import PetsLove.sistema.dados.RepositorioUsuarios;
 import PetsLove.sistema.negocios.ControladorUsuario;
+import PetsLove.sistema.negocios.beans.Animal;
 import PetsLove.sistema.negocios.beans.Usuario;
 
 public class FachadaPL {
@@ -31,6 +34,34 @@ public class FachadaPL {
 	
 	public void atualizarUsuario(Usuario usuario) {
 		this.controladorUsuario.atualizar(usuario);
+	}
+	
+	public ArrayList<Usuario> listarUsuarios() {
+		return this.controladorUsuario.listar();
+	}
+	
+	public ArrayList<Animal> listarAnimais(){
+		return this.controladorUsuario.listarAnimais();
+	}
+	
+	public ArrayList<Animal> listarAnimaisPorTipo(Class<?> tipo){
+		return this.controladorUsuario.listarAnimaisPorTipo(tipo);
+	}
+	
+	public ArrayList<Animal> listarCachorrosPorRaca(String raca){
+		return this.controladorUsuario.listarCachorrosPorRaca(raca);
+	}
+	
+	public ArrayList<Animal> listarCachorrosPorTamanho(String tamanho){
+		return this.controladorUsuario.listarCachorrosPorTamanho(tamanho);
+	}
+	
+	public ArrayList<Animal> listarCachorrosPorRacaETamanho(String raca, String tamanho){
+		return this.controladorUsuario.listarCachorrosPorRacaETamanho(raca, tamanho);
+	}
+	
+	public ArrayList<Animal> listarGatosPorPelagem(String pelagem){
+		return this.controladorUsuario.listarGatosPorPelagem(pelagem);
 	}
 	
 }
--- src/PetsLove/sistema/negocios/ControladorUsuario.java
+++ src/PetsLove/sistema/negocios/ControladorUsuario.java
@@ -1,6 +1,11 @@
 package PetsLove.sistema.negocios;
 
+import java.util.ArrayList;
+
 import PetsLove.sistema.dados.IRepositorioUsuarios;
+import PetsLove.sistema.negocios.beans.Animal;
+import PetsLove.sistema.negocios.beans.Cachorro;
+import PetsLove.sistema.negocios.beans.Gato;
 import PetsLove.sistema.negocios.beans.Usuario;
 
 public class ControladorUsuario {
@@ -22,6 +27,74 @@ public class ControladorUsuario {
 	
 	public void atualizar(Usuario usuario) {
 		this.repositorioUsuarios.atualizar(usuario);
+	}
+	
+	public ArrayList<Usuario> listar(){
+		return this.repositorioUsuarios.listar();
+	}
+
+	public ArrayList<Animal> listarAnimais() {
+		ArrayList<Usuario> usuarios = this.listar();
+		ArrayList<Animal> animais = new ArrayList<Animal>();
+		for(Usuario user : usuarios) {
+			for(Animal animal : user.getAnimais()) {
+				animais.add(animal);
+			}
+		}
+		return animais;
+	}
+	
+	public ArrayList<Animal> listarAnimaisPorTipo(Class<?> tipo) {
+		ArrayList<Usuario> usuarios = this.listar();
+		ArrayList<Animal> animais = new ArrayList<Animal>();
+		for(Usuario user : usuarios) {
+			for(Animal animal : user.getAnimais()) {
+				if(animal.getClass() == tipo) {
+					animais.add(animal);
+				}
+			}
+		}
+		return animais;
+	}
+	
+	public ArrayList<Animal> listarCachorrosPorRaca(String raca) {
+		ArrayList<Animal> cachorros = this.listarAnimaisPorTipo(Cachorro.class);
+		for(Animal cachorro : cachorros) {
+			if(((Cachorro) cachorro).getRaca().equals(raca)) {
+				cachorros.add(cachorro);
+			}
+		}
+		return cachorros;
+	}
+	
+	public ArrayList<Animal> listarCachorrosPorTamanho(String tamanho) {
+		ArrayList<Animal> cachorros = this.listarAnimaisPorTipo(Cachorro.class);
+		for(Animal cachorro : cachorros) {
+			if(((Cachorro) cachorro).getTamanho().equals(tamanho)) {
+				cachorros.add(cachorro);
+			}
+		}
+		return cachorros;
+	}
+	
+	public ArrayList<Animal> listarCachorrosPorRacaETamanho(String raca, String tamanho) {
+		ArrayList<Animal> cachorros = this.listarCachorrosPorRaca(raca);
+		for(Animal cachorro : cachorros) {
+			if(!((Cachorro) cachorro).getTamanho().equals(tamanho)) {
+				cachorros.remove(cachorro);
+			}
+		}
+		return cachorros;
+	}
+	
+	public ArrayList<Animal> listarGatosPorPelagem(String pelagem) {
+		ArrayList<Animal> gatos = this.listarAnimaisPorTipo(Gato.class);
+		for(Animal gato : gatos) {
+			if(((Gato) gato).getPelagem().equals(pelagem)) {
+				gatos.add(gato);
+			}
+		}
+		return gatos;
 	}
 
 }
