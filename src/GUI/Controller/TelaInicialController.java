37ef086d8f8003e7a0d619bf97da1538cff7b032
Cvalca <51674221+Cvalca@users.noreply.github.com>
2019-11-17 16:59:50 -0300

    Adicionando classes iniciais para gui

---
+++ src/GUI/Controller/TelaInicialController.java
@@ -0,0 +1,38 @@
+package GUI.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+
+
+public class TelaInicialController {
+
+	
+
+	    @FXML
+	    private PasswordField DigSenha;
+
+	    @FXML
+	    private Label email;
+
+	    @FXML
+	    private Label Senha;
+
+	    @FXML
+	    private Button cadastrese;
+
+	    @FXML
+	    private ImageView logo;
+
+	    @FXML
+	    private Label labelSemConta;
+
+	    @FXML
+	    private TextField digEmail;
+
+}
+
+
