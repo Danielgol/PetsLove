7170e79b16166d758c6b4579df757f049a9182eb
Cvalca <51674221+Cvalca@users.noreply.github.com>
2019-11-18 21:19:06 -0300

    Arrumando as classes controllers

--- src/GUI/Controller/LoginController.java
+++ src/GUI/Controller/LoginController.java
@@ -3,12 +3,9 @@ package GUI.Controller;
 import GUI.MainApp;
 import PetsLove.sistema.FachadaPL;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+
 
 public class LoginController {
 
@@ -16,31 +13,17 @@ public class LoginController {
 	private FachadaPL fachada = FachadaPL.getInstance();
 	
 
-    @FXML
-    private Pane painelPrincipal;
-
-    @FXML
-    private ImageView Logo;
-
-    @FXML
-    private Label email;
-
-    @FXML
-    private TextField digitarEmail;
-
-    @FXML
-    private Label senha;
-
-    @FXML
-    private PasswordField digitarSenha;
-
-    @FXML
-    private Button botaoEntrar;
-
-    @FXML
-    private Label labelSemConta;
-
-    @FXML
-    private Button botaoCadastrar;
 	
+
+	    @FXML
+	    private TextField tfEmail;
+
+	    @FXML
+	    private PasswordField tfSenha;
+
+	@FXML
+	public void handleCadastrese()
+	{
+		
+	}
 }
