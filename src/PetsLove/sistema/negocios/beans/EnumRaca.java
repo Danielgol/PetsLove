683d7394f6dd8d6078333aee8b2990eae047c120
Wei <mateuswei@hotmail.com>
2019-11-21 22:39:35 -0200

    Adição de algumas raças

--- src/PetsLove/sistema/negocios/beans/EnumRaca.java
+++ src/PetsLove/sistema/negocios/beans/EnumRaca.java
@@ -2,6 +2,7 @@ package PetsLove.sistema.negocios.beans;
 
 public enum EnumRaca {
 	
+	//Cachorro
 	PITBULL("Pitbull"),
 	PASTORALEMAO("Pastor Alemao"),
 	BEAGLE("Beagle"),
@@ -20,6 +21,35 @@ public enum EnumRaca {
 	DOBERMANN("Dobermann"),
 	ROTTWEILER("Rottweiler"),
 	PUG("Pug"),
+	LHASAAPSO("Lhasa Apso"),
+	MALTES("Maltes"),
+	GOLDEN("Golden"),
+	SHIHTZU("Shih-tzu"),
+	BORDERCOLLIE("Border Collie"),
+	
+	//Gato
+	ABISSINIO("Abissinio"),
+	ANGORA("Angora"),
+	BALINES("Balines"),
+	BENGAL("Bengal"),
+	BOMBAY("Bombay"),
+	BURMES("Burmes"),
+	CHARTREUX("Chartreux"),
+	HIMALAIA("Himalaia"),
+	JAGUATIRICA("Jaguatirica"),
+	LAPERM("LaPerm"),
+	MAINECOON("Maine Coon"),
+	CYMRIC("Cymric"),
+	PERSA("Persa"),
+	RAGDOLL("Ragdoll"),
+	OCICAT("Ocicat"),
+	SAGRADODABIRMANIA("Sagrado da Birmania"),
+	SAVANNAH("Savannah"),
+	SIAMES("Siames"),
+	SPHYNX("Sphynx"),
+	THAI("Thai"),
+	SIBERIANO("Siberiano"),
+	BRITISHSHORTHAIR("British Shorthair"),
 	INDEFINIDO("Indefinido");
 	
 	public String valor;
