81b03842c1f727b4e611cad1def6d8fe21d3bd8a
Wei <mateuswei@hotmail.com>
2019-11-27 01:55:21 -0200

    Restrições na alteração de dados do cachorro

--- src/GUI/Controller/EditarCachorroController.java
+++ src/GUI/Controller/EditarCachorroController.java
@@ -4,6 +4,8 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ResourceBundle;
+
+import GUI.System.CadastrarCachorroApp;
 import GUI.System.EditarCachorroApp;
 import GUI.System.SeusAnimaisApp;
 import PetsLove.sistema.FachadaPL;
@@ -16,28 +18,38 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
-public class EditarCachorroController implements Initializable{
+public class EditarCachorroController implements Initializable {
 
 	public static Cachorro selecionado;
 
-	@FXML private TextArea taDescricao;
-	@FXML private TextField tfNome;
-	@FXML private DatePicker dpDataDeNascimento;
-	@FXML private ComboBox<String> cbSexo;
-	@FXML private ComboBox<String> cbPorte;
-	@FXML private ComboBox<String> cbRaca;
+	@FXML
+	private TextArea taDescricao;
+	@FXML
+	private TextField tfNome;
+	@FXML
+	private DatePicker dpDataDeNascimento;
+	@FXML
+	private ComboBox<String> cbSexo;
+	@FXML
+	private ComboBox<String> cbPorte;
+	@FXML
+	private ComboBox<String> cbRaca;
 
 	ObservableList<String> sexo = FXCollections.observableArrayList(EnumSexo.MACHO.valor, EnumSexo.FEMEA.valor);
-	ObservableList<String> porte = FXCollections.observableArrayList(EnumPorte.GRANDE.valor, EnumPorte.PEQUENO.valor, EnumPorte.MEDIO.valor);
+	ObservableList<String> porte = FXCollections.observableArrayList(EnumPorte.GRANDE.valor, EnumPorte.PEQUENO.valor,
+			EnumPorte.MEDIO.valor);
 	ObservableList<String> raca = FXCollections.observableArrayList(EnumRacaCachorro.getValues());
-	
-	//TODO: Quando carregar a tela, colocar automaticamente os dados atuais do animal nos campos
+
+	// TODO: Quando carregar a tela, colocar automaticamente os dados atuais do
+	// animal nos campos
 
 	@FXML
 	void handleCancelar() {
@@ -54,61 +66,79 @@ public class EditarCachorroController implements Initializable{
 	@FXML
 	void handleSalvar() {
 
-		int idade = Period.between(dpDataDeNascimento.getValue(), LocalDate.now()).getYears();
+		if (tfNome.getText().equals("")
+				|| !tfNome.getText().matches("^[A-Za-z·‡‚„ÈËÍÌÔÛÙıˆ˚˙ÁÒ¡¿¬√… »Õœ”‘’÷⁄€«— ]+$")) {
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setHeaderText("Erro ao cadastrar");
+			alerta.setTitle("Erro");
+			alerta.setContentText("Nome inv·lido");
+			alerta.show();
+		} else if (dpDataDeNascimento.getValue() == null) {
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setHeaderText("Erro ao cadastrar");
+			alerta.setTitle("Erro");
+			alerta.setContentText("Data de nascimento inv·lida");
+			alerta.show();
+		} else if (cbSexo.getValue() == null) {
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setHeaderText("Erro ao cadastrar");
+			alerta.setTitle("Erro");
+			alerta.setContentText("Sexo inv·lido");
+			alerta.show();
+		} else if (cbRaca.getValue() == null) {
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setHeaderText("Erro ao cadastrar");
+			alerta.setTitle("Erro");
+			alerta.setContentText("RaÁa inv·lida");
+			alerta.show();
+		} else if (cbPorte.getValue() == null) {
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setHeaderText("Erro ao cadastrar");
+			alerta.setTitle("Erro");
+			alerta.setContentText("Porte inv·lido");
+			alerta.show();
+		} else {
+			int idade = Period.between(dpDataDeNascimento.getValue(), LocalDate.now()).getYears();
 
-		EnumSexo sexo = null;
-		if(cbSexo.getValue().equals(EnumSexo.MACHO.valor)){
-			sexo = EnumSexo.MACHO;
-		}else if(cbSexo.getValue().equals(EnumSexo.FEMEA.valor)){
-			sexo = EnumSexo.FEMEA;
-		}
+			EnumSexo sexo = EnumSexo.MACHO;
+			if (cbSexo.getValue().equals(EnumSexo.FEMEA.valor)) {
+				sexo = EnumSexo.FEMEA;
+			}
+			EnumPorte porte = EnumPorte.getPorte(cbPorte.getValue());
+			EnumRacaCachorro raca = EnumRacaCachorro.getRaca(cbRaca.getValue());
 
-		EnumPorte porte = null;
-		for(EnumPorte e: EnumPorte.values()){
-			if(e.valor.equals(cbPorte.getValue())){
-				porte = e;
+			selecionado.setNome(tfNome.getText());
+			selecionado.setIdade(idade);
+			selecionado.setDescricao(taDescricao.getText());
+			selecionado.setRaca(raca);
+			selecionado.setPorte(porte);
+			selecionado.setSexo(sexo);
+
+			FachadaPL.getInstance().atualizarAnimal(selecionado);
+			SeusAnimaisApp seusAnimais = new SeusAnimaisApp();
+			EditarCachorroApp.getStage().close();
+
+			try {
+				seusAnimais.start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-
-		EnumRacaCachorro raca = null;
-		for(EnumRacaCachorro e: EnumRacaCachorro.values()){
-			if(e.valor.equals(cbRaca.getValue())){
-				raca = e;
-			}
-		}
-
-		if(selecionado == null){
-			//TODO: Tirar esse pritln ladkoaekdoadlaedk
-			System.out.println("NULOOOOOOOOOOOOOOOOOO IDIOTA");
-		}
-		
-		//TODO: Colocar Regras de Cadastro (Todos os campos devem ser preenchidos)
-
-		selecionado.setNome(tfNome.getText());
-		selecionado.setIdade(idade);
-		selecionado.setDescricao(taDescricao.getText());
-		selecionado.setRaca(raca);
-		selecionado.setPorte(porte);
-		selecionado.setSexo(sexo);
-
-		FachadaPL.getInstance().atualizarAnimal(selecionado);
-
-		SeusAnimaisApp seusAnimais = new SeusAnimaisApp();
-		EditarCachorroApp.getStage().close();
-
-		try {
-			seusAnimais.start(new Stage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		/*
+		 * nao entendi pra que serve isso 
+		 * 
+		 * if(selecionado == null){ //TODO: Tirar esse pritln ladkoaekdoadlaedk 
+		 * System.out.println("NULOOOOOOOOOOOOOOOOOO IDIOTA");
+		 * }
+		 */
 	}
 
 	public static void setSelecionado(Animal animal) {
-		if(FachadaPL.getUsuarioLogado().equals(FachadaPL.getInstance().procurarUsuario(animal.getEmailDono()))) {
+		if (FachadaPL.getUsuarioLogado().equals(FachadaPL.getInstance().procurarUsuario(animal.getEmailDono()))) {
 			selecionado = (Cachorro) animal;
 		}
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		cbSexo.setItems(sexo);
