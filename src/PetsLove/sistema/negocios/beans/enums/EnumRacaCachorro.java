bb0a575d785172cbc49322d5d538f69365236e6c
Wei <mateuswei@hotmail.com>
2019-11-27 23:30:48 -0200

    Adição de mais raças e cadastro de alguns animais

--- src/PetsLove/sistema/negocios/beans/enums/EnumRacaCachorro.java
+++ src/PetsLove/sistema/negocios/beans/enums/EnumRacaCachorro.java
@@ -29,6 +29,19 @@ public enum EnumRacaCachorro {
 	GOLDEN("Golden"),
 	SHIHTZU("Shih-tzu"),
 	BORDERCOLLIE("Border Collie"),
+	ZWERGSPITZ("Zwergspitz"),
+	SHIBAINU("Shiba inu"),
+	CHOWCHOW("Chow Chow"),
+	SAMOIEDA("Samoieda"),
+	MASTIMINGLES("Mastim ingles"),
+	CANECORSO("Cane Corso"),
+	CORGI("Corgi"),
+	BULLTERRIER("Bull Terrier"),
+	SHARPEI("Shar pei"),
+	CAVALIER("Cavalier"),
+	AMERICANBULLY("American Bully"),
+	BOSTONTERRIER("Boston Terrier"),
+	JACKRUSSELL("Jack Russell"),
 	INDEFINIDO("Indefinido");
 	
 	public String valor;
